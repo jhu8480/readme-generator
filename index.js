fcbc41d908ab8ddda027260e254f0084ed628b26
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-06 21:32:08 -0500

    added list choices

--- index.js
+++ index.js
@@ -8,17 +8,47 @@ const questions = [
     type: 'input',
     message: 'What is the name of your project?',
     name: 'title'
+  },
+  {
+    type: 'input',
+    message: 'Please input the description of the project',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'Please type in the installation instructions',
+    name: 'installation'
+  },
+  {
+    type: 'list',
+    message: 'Please choose liscence type',
+    name: 'liscence',
+    choices: ['MIT License', 'Apache License 2.0', 'Mozilla Public License 2.0', 'Boost Software License 1.0', 'Eclipse Public License 2.0']
   }
 ];
 
 
 prompt(questions).then(
   async (response) => {
-    await fsPro.mkdir('./files')
-    await fsPro.writeFile(`./files/readme.md`, `# ${response.title}\n`, (err) => {
+    const readmeFile = `./files/readme.md`;
+    await fsPro.mkdir('./files');
+    
+    await fsPro.writeFile(readmeFile, `# ${response.title}\n`, (err) => {
       if (err) throw err;
-      else console.log('title added to readme');
-    })
+      else console.log('title added to readme.md');
+    });
+
+    await fsPro.appendFile(readmeFile, `\n\n## Description \n${response.description}`, (err) => {
+      if (err) throw err;
+      else console.log('Description added to readme.md');
+    });
+
+    await fsPro.appendFile(readmeFile, `\n\n## Installation \n${response.installation}`, (err) => {
+      if (err) throw err;
+      else console.log('Installation instructions added to readme.md');
+    });
+
+    
   }
 );
 
