163791fe76bd25668fdd9cf742f26b6d1dde91ea
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-07 17:53:55 -0500

    added anchors to attach sections to table of contents

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//Import modules
 const fs = require('fs');
 const fsPro = require('fs').promises;
 const inquirer = require('inquirer');
 
-
+// get the questions object from questions.json
 const questions = JSON.parse(fs.readFileSync('./questions.json', 'utf8'));
+
+// start the prompts
 const prompt = inquirer.createPromptModule();
-  prompt(questions).then(
+prompt(questions).then(
   async (response) => {
     const readmeFile = `./files/readme.md`;
     await fsPro.mkdir('./files');
@@ -13,29 +16,29 @@ const prompt = inquirer.createPromptModule();
       if (err) throw err;
     });
 
-    await fsPro.appendFile(readmeFile, `\n\n## Table of Contents\n\t - license\n\t - Description\n\t - Installation\n\t - Usage\n\t - Contributing\n\t - Questions`)
+    await fsPro.appendFile(readmeFile, `\n\n## Table of Contents\n\t 1. [license](#license)\n\t 2. [Description](#description)\n\t 3. [Installation](#installation)\n\t 4. [Usage](#usage)\n\t 5. [Contributing](#contributing)\n\t 6. [Questions](#questions)`)
 
     const licenseBadge = `[![License](https://img.shields.io/badge/License-${response.license}-blue.svg)]`;
 
-    await fsPro.appendFile(readmeFile, `\n\n## License \n\t${response.license} \n\t${licenseBadge}`);
+    await fsPro.appendFile(readmeFile, `\n\n## License <a name="license"></a> \n\t${response.license} \n\t${licenseBadge}`);
 
-    await fsPro.appendFile(readmeFile, `\n\n## Description \n\t${response.description}`, (err) => {
+    await fsPro.appendFile(readmeFile, `\n\n## Description <a name="description"></a>\n\t${response.description}`, (err) => {
       if (err) throw err;
     });
 
-    await fsPro.appendFile(readmeFile, `\n\n## Installation \n\t${response.installation}`, (err) => {
+    await fsPro.appendFile(readmeFile, `\n\n## Installation <a name="installation"></a>\n\t${response.installation}`, (err) => {
       if (err) throw err;
     });
 
-    await fsPro.appendFile(readmeFile, `\n\n## Usage \n\t${response.usage}`, (err) => {
+    await fsPro.appendFile(readmeFile, `\n\n## Usage <a name="usage"></a>\n\t${response.usage}`, (err) => {
       if (err) throw err;
     });
 
-    await fsPro.appendFile(readmeFile, `\n\n## Contributing \n\t${response.contributing}`, (err) => {
+    await fsPro.appendFile(readmeFile, `\n\n## Contributing <a name="contributing"></a>\n\t${response.contributing}`, (err) => {
       if (err) throw err;
     });
 
-    await fsPro.appendFile(readmeFile, '\n\n## Questions \n\t Where are my projects stored?')
+    await fsPro.appendFile(readmeFile, '\n\n## Questions <a name="questions"></a>\n\t Where are my projects stored?')
 
     await fsPro.appendFile(readmeFile, `\n\t - link to github profile: https://github.com/${response.github}/`, (err) => {
       if (err) throw err;
